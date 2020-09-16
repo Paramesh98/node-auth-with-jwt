0e4e88065ea2559f2a05478e038a42e4eebd808b
Paramesh Krishna <parameshkrishna98@gmail.com>
2020-09-16 19:12:15 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
+const cookieParser = require("cookie-parser");
+const { requireAuth, checkUser } = require("./middleware/authMiddleware");
+
+const app = express();
+
+// middleware
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use(cookieParser());
+
+// view engine
+app.set("view engine", "ejs");
+
+// database connection
+const dbURI =
+  "mongodb+srv://param:param12345@cluster0.1yguy.mongodb.net/node-auth";
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// routes
+app.get("*", checkUser);
+app.get("/", (req, res) => res.render("home"));
+app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
+app.use(authRoutes);
+
+app.get("/set-cookies", (req, res) => {});
+app.get("/read-cookies", (req, res) => {});
